4822e2d12b49477ee9a4020cb2a87544dab50fdd
Alex Lee <alex@uniteus.com>
2020-04-02 19:25:36 -0400

    Authentication via blockstack/connect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Connect, useConnect } from '@blockstack/connect';
+import ReactBlockstack, { useBlockstack, didConnect } from 'react-blockstack';
+import { AppConfig } from 'blockstack';
+
+const appConfig = new AppConfig(['store_write', 'publish_data']);
+ReactBlockstack({ appConfig });
+
+function SignedInPage(props) {
+  return (
+    <div>
+      <h1>You are Signed In</h1>
+      <button onClick={props.signOut}>
+        Sign Out
+      </button>
+    </div>
+  )
+}
+
+function LoginPage() {
+  const { doOpenAuth } = useConnect();
+
+  return (
+    <div>
+      <h1>You must sign up or log in</h1>
+      <button onClick={() => doOpenAuth(false)}>
+        Sign Up
+      </button>
+      <button onClick={() => doOpenAuth(true)}>
+        Sign In
+      </button>
+    </div>
+  )
+}
 
 function App() {
+  const { userSession, authenticated, signOut } = useBlockstack();
+
+  const authOptions = {
+    redirectTo: '/',
+    finished: ({ userSession }) => {
+      didConnect({ userSession })
+    },
+    appDetails: {
+      name: 'tech-rally-connect-blockstack',
+      icon: 'https://tech-rally-test-bucket.s3.us-east-2.amazonaws.com/Profile_Picture.png',
+    },
+    userSession,
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Connect authOptions={authOptions}>
+        {
+          authenticated ? (
+            <SignedInPage signOut={signOut} />
+          ) : (
+            <LoginPage />
+          )
+        }
+      </Connect>
     </div>
   );
 }
